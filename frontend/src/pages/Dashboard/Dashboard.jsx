92fc93ba0684dfbb5d3643e7d0f8f89be888751e
MohamedGrissa0 <mouhamedgrissa0@gmail.com>
2023-03-16 14:29:32 +0100

    nn

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -15,13 +15,13 @@ export default function Dashboard() {
    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
       <div class="grid grid-cols-3 gap-4 mb-4">
          <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+            <p class="text-2xl text-gray-400 dark:text-gray-500">nb of users</p>
          </div>
          <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+            <p class="text-2xl text-gray-400 dark:text-gray-500">nb of posts</p>
          </div>
          <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">+</p>
+            <p class="text-2xl text-gray-400 dark:text-gray-500">nb of reviews</p>
          </div>
       </div>
       <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
