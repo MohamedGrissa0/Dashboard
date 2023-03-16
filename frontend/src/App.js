0ec0b1c36dc184e7037ec88e062e8582cc9420c8
Ismail chebaane <chebaaneismail@gmail.com>
2023-03-16 14:05:09 +0100

    l

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './pages/Dashboard/Dashboard';
-
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
 function App() {
   return (
-    <div className="App">
-    <Dashboard />
-    </div>
+    <Router >
+      <Navbar />
+      <Routes>
+      <Route exact path="/" element={ <Dashboard />} />
+        
+      </Routes>
+   
+    </Router>
   );
 }
 
