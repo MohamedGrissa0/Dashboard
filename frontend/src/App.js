ea44d421680abb71cea7037c10b3f6b7951c519f
Ismail chebaane <chebaaneismail@gmail.com>
2023-03-16 15:42:49 +0100

    l

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import Form from './pages/Form/Form';
+import Users from './pages/Users/Users';
+import Places from './pages/Places/Places';
+import Settings from './pages/Settings/Settings';
+import Review from './pages/Review/Review';
 function App() {
   return (
     <Router >
       <Navbar />
       <Routes>
-      <Route exact path="/" element={ <Dashboard />} />
         
+      <Route  path="/reviews" element={ <Review />} />
+      <Route  path="/settings" element={ <Settings />} />
+      <Route  path="/users" element={ <Users />} />
+      <Route exact path="/" element={ <Dashboard />} />
+       <Route  path="/places" element={ <Places />} />
+      <Route  path="/form" element={ <Form />} />
       </Routes>
    
     </Router>
