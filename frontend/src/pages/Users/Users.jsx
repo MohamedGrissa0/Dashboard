3e7fa7e5bc5b30b9c8e1d736af5cf094cfe45952
MohamedGrissa0 <mouhamedgrissa0@gmail.com>
2023-03-20 08:05:46 +0100

    b

--- frontend/src/pages/Users/Users.jsx
+++ frontend/src/pages/Users/Users.jsx
@@ -119,13 +119,7 @@ const UsersTable = () => {
       <input type="email" id="email" name="email" placeholder="Enter your email" className="block w-full border-2 border-gray-400 rounded-lg bg-transparent text-gray-700 py-2 px-4 md:py-3 md:px-5 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" required />
     </div>
   </div>
-  <div className="relative mb-3">
-    <label htmlFor="message" className="block text-gray-700 font-bold mb-2">
-      <i className="far fa-comment mr-2"></i>
-      Message
-    </label>
-    <textarea id="message" name="message" placeholder="Enter your message" className="block w-full border-2 border-gray-400 rounded-lg bg-transparent text-gray-700 py-2 px-4 md:py-3 md:px-5 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" rows="4" required></textarea>
-  </div>
+  
   <div className="flex justify-center">
     <button type="button" onClick={() => setisclicked(false)} className="bg-blue-500 text-white text-lg px-6 py-2 rounded-lg mr-4 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2 transition-all duration-200 ease-in-out">Cancel</button>
     <button type="button" className="bg-blue-500 text-white text-lg px-6 py-2 rounded-lg mr-4 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2 transition-all duration-200 ease-in-out">Done</button>
