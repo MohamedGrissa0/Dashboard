fa0b84892050b6b7aa07ee69a542181c7edf1a43
MohamedGrissa0 <mouhamedgrissa0@gmail.com>
2023-03-20 08:04:23 +0100

    b

--- frontend/src/pages/Users/Users.jsx
+++ frontend/src/pages/Users/Users.jsx
@@ -5,6 +5,7 @@ const UsersTable = () => {
     const [email, setEmail] = useState("");
     const [isclicked, setisclicked] = useState(false)
     const [isDone , setisDone] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -103,18 +104,27 @@ const UsersTable = () => {
   <div>
   <form onSubmit={handleSubmit} className={isclicked ? "p-3 absolute top-[35%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-999 bg-gray-500 rounded-lg shadow-xl w-11/12 md:w-2/3 lg:w-1/2" : "hidden"}>
   <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-3">
-    <div className="relative">
-      <label htmlFor="name" className="block text-white text-lg mb-2">Name:</label>
-      <input type="text" id="name" name="name" placeholder="Enter your name" className="block w-full border-2 border-white rounded-lg bg-transparent text-white py-2 px-4 md:py-3 md:px-5 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" required />
+    <div className="relative flex flex-col justify-between">
+      <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
+        <i className="far fa-user mr-2"></i>
+        Name
+      </label>
+      <input type="text" id="name" name="name" placeholder="Enter your name" className="block w-full border-2 border-gray-400 rounded-lg bg-transparent text-gray-700 py-2 px-4 md:py-3 md:px-5 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" required />
     </div>
-    <div className="relative">
-      <label htmlFor="email" className="block text-white text-lg mb-2">Email:</label>
-      <input type="email" id="email" name="email" placeholder="Enter your email" className="block w-full border-2 border-white rounded-lg bg-transparent text-white py-2 px-4 md:py-3 md:px-5 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" required />
+    <div className="relative flex flex-col justify-between">
+      <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+        <i className="far fa-envelope mr-2"></i>
+        Email
+      </label>
+      <input type="email" id="email" name="email" placeholder="Enter your email" className="block w-full border-2 border-gray-400 rounded-lg bg-transparent text-gray-700 py-2 px-4 md:py-3 md:px-5 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" required />
     </div>
   </div>
   <div className="relative mb-3">
-    <label htmlFor="message" className="block text-white text-lg mb-2">Message:</label>
-    <textarea id="message" name="message" placeholder="Enter your message" className="block w-full border-2 border-white rounded-lg bg-transparent text-white py-2 px-4 md:py-3 md:px-5 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" rows="4" required></textarea>
+    <label htmlFor="message" className="block text-gray-700 font-bold mb-2">
+      <i className="far fa-comment mr-2"></i>
+      Message
+    </label>
+    <textarea id="message" name="message" placeholder="Enter your message" className="block w-full border-2 border-gray-400 rounded-lg bg-transparent text-gray-700 py-2 px-4 md:py-3 md:px-5 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" rows="4" required></textarea>
   </div>
   <div className="flex justify-center">
     <button type="button" onClick={() => setisclicked(false)} className="bg-blue-500 text-white text-lg px-6 py-2 rounded-lg mr-4 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2 transition-all duration-200 ease-in-out">Cancel</button>
