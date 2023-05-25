1312fbaa9780b1c9ba87d788e2fd6ee12c6167ce
MohamedGrissa0 <mouhamedgrissa0@gmail.com>
2023-05-25 10:06:36 +0100

    h

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -88,7 +88,6 @@ export default function Login({ location }) {
     </div>
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Login</button>
     <div class="flex flex-row justify-center">
-      <h6 class="mr-2">New to ReviewHub?</h6>
       <Link class="text-blue-500 font-bold underline" to="/register">Sign Up</Link>
     </div>
   </form>
